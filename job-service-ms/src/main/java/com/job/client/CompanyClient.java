bbfed501228734610dfe6022ae677491eff04369
Shaneen <jawalinikita06@gmail.com>
2024-05-19 23:02:58 +0530

    changes added

--- job-service-ms/src/main/java/com/job/client/CompanyClient.java
+++ job-service-ms/src/main/java/com/job/client/CompanyClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.job.external.Company;
 
-@FeignClient(name = "COMPANY-SERVICE")
+@FeignClient(name = "company-service")
 public interface CompanyClient {
 
 	@GetMapping("/company/findCompanyById/{companyId}")
